8960d72498cc7e95e34137542da332bec54b110e
igorskiter <igor.nw3@gmail.com>
2024-08-27 00:01:39 -0300

    feat: adjusting import-meta to jest

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -2,13 +2,37 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.app.json" }],
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.app.json",
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    // Replicate as .env.local
+                    VITE_API_URL: 'http://localhost:3001',
+                  },
+                },
+              },
+            },
+          ],
+        }
+      }
+    ],
   },
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/dist/"
   ],
   moduleFileExtensions: [
+    'node',
     "ts",
     "tsx",
     "js",
@@ -19,6 +43,7 @@ export default {
     "^.+\\.svg$": "jest-transformer-svg",
     "^@/components(.*)$": "<rootDir>/components/$1",
     "^@/features(.*)$": "<rootDir>/features/$1",
+    "^@/hooks(.*)$": "<rootDir>/hooks/$1",
     "^@/interfaces(.*)$": "<rootDir>/interfaces/$1",
     "^@/lib(.*)$": "<rootDir>/lib/$1",
     "^@/(.*)$": "<rootDir>/src/$1",
