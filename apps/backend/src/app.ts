0afd3121faaf6ccf5fe0e7d8290da1b1f4ed63e3
igorskiter <igor.nw3@gmail.com>
2024-08-27 00:21:00 -0300

    feat: adding cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,8 +4,12 @@ import frames from "./controllers/frames";
 const express = require("express");
 const bodyParser = require("body-parser");
 const { sequelize } = require("./model");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
 app.use(bodyParser.json());
 app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
