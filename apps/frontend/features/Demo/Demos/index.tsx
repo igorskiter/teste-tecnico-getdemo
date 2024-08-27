a2ae744a96facc6365860ddc20aa0f76ab2b1632
igorskiter <igor.nw3@gmail.com>
2024-08-26 21:05:25 -0300

    feat: refactoring to use nested route in DemoDetail

--- apps/frontend/features/Demo/Demos/index.tsx
+++ apps/frontend/features/Demo/Demos/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Demo } from "@/interfaces/Demo";
 import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 function Demos() {
   const [demos, setDemos] = useState<Demo[] | []>([]);
 
@@ -19,16 +20,24 @@ function Demos() {
   return (
     <div className="flex flex-col w-full h-full p-8 box-border">
       <h1 className="text-2xl">List demos</h1>
-      {demos.length > 0 &&
-        demos.map((demo) => (
-          <div
-            key={demo.id}
-            className="p-4 rounded-lg border border-[#646cff] max-w-64 flex flex-col justify-center items-center"
-          >
-            <h2>{demo.name}</h2>
-            <Button>Visualizar</Button>
-          </div>
-        ))}
+      <div className="flex flex-row flex-1">
+        <div className="flex flex-1 w-1/3">
+          {demos.length > 0 &&
+            demos.map((demo) => (
+              <div
+                key={demo.id}
+                className="p-4 rounded-lg border border-[#646cff] max-w-64 flex flex-col justify-center items-center"
+              >
+                <h2>{demo.name}</h2>
+                <Button>Visualizar</Button>
+              </div>
+            ))}
+        </div>
+
+        <div className="flex flex-2 ml-12 w-2/3">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -1,15 +1,23 @@
 import DemoDetail from "@/features/Demo/DemoDetail";
 import Demos from "@/features/Demo/Demos";
+import SelectDemoMessage from "@/features/Demo/Demos/SelectDemoMessage";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Demos />} />
-        <Route path="/demos" element={<Demos />} />
-        <Route path="/demos/detail/:frameId" element={<DemoDetail />} />
+        <Route path="/demos" element={<Demos />}>
+          <Route index element={<SelectDemoMessage />} />
+          <Route path="detail/:demoId" element={<DemoDetail />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/demos" />} />
       </Routes>
     </Router>
   );
