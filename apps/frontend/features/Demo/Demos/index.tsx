3e18e1dde5ddc7579ac0456a4583e0318bf08689
igorskiter <igor.nw3@gmail.com>
2024-08-26 22:02:38 -0300

    feat: adding feature flag to add demo

--- apps/frontend/features/Demo/Demos/index.tsx
+++ apps/frontend/features/Demo/Demos/index.tsx
@@ -5,6 +5,9 @@ import { Demo } from "@/interfaces/Demo";
 import { PlusIcon, SearchIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+
+const ENABLE_ADD_DEMO = false;
+
 function Demos() {
   const [demos, setDemos] = useState<Demo[] | []>([]);
 
@@ -28,24 +31,24 @@ function Demos() {
             <h1 className="text-2xl font-bold text-black dark:text-white">
               Demos
             </h1>
-            <Button size="icon" variant="ghost">
-              <PlusIcon className="text-black dark:text-white w-6 h-6" />
-            </Button>
+            {ENABLE_ADD_DEMO && (
+              <Button size="icon" variant="ghost">
+                <PlusIcon className="text-black dark:text-white w-6 h-6" />
+              </Button>
+            )}
           </div>
           <div className="relative">
             <SearchIcon className="absolute left-2.5 top-3 h-4 w-4 text-zinc-500 dark:text-zinc-400" />
             <Input
               className="pl-8"
-              placeholder="Search messages..."
+              placeholder="Search demos..."
               type="search"
             />
           </div>
           <div className="space-y-2 h-full">
             {demos.length === 0 && (
               <div className="flex items-center justify-center w-full h-full">
-                <h3 className="text-sm w-full">
-                  Nenhum demo encotrado!
-                </h3>
+                <h3 className="text-sm w-full">Nenhum demo encotrado!</h3>
               </div>
             )}
             {demos.length > 0 &&
