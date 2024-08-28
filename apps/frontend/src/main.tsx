691bb91282ea1027bb1ecacae3849fea0f5e3786
igorskiter <igor.nw3@gmail.com>
2024-08-28 12:08:45 -0300

    feat: adding persistence queryClinet in react-query

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,7 +5,15 @@ import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
